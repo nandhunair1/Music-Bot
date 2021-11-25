3181bdb815f4b17897537bfd6d39402c5329025e
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-25 08:30:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", "6"))
 API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
+DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
 
 if __name__ == "__main__" :
     plugins = dict(
